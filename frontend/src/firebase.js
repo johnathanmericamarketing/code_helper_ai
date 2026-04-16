70e67a2f335afe8b018bf2e3c7bb0b450c77e835
johnathanmericamarketing <johnathan.mericamarketing@gmail.com>
2026-04-16 13:29:56 -0700

    feat: add Firebase config and initial project setup

---
+++ frontend/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzHJ_TTNcwSuGNGfMh1mJ_xVI1PcMhuE0",
+  authDomain: "code-helper-studio.firebaseapp.com",
+  projectId: "code-helper-studio",
+  storageBucket: "code-helper-studio.firebasestorage.app",
+  messagingSenderId: "502657160656",
+  appId: "1:502657160656:web:658a88c5722c8ab28ed5d5",
+  measurementId: "G-80R9E2P221"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
